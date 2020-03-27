077d8a1f56f9f8fb36fe86d4c6a1689fbb0b3aa2
Arjun <53340883+arjun-vl@users.noreply.github.com>
2020-03-27 22:38:21 +0400

    Update existing check wvd.ps1

--- existing check wvd.ps1
+++ existing check wvd.ps1
@@ -1,15 +1,9 @@
-﻿
 
-##Azure AD ID   a86bc255-9bb7-4ee8-b30a-51fba84872aa
-##Azure Subscription ID   aec3f8b4-cf54-41d2-bc4a-1c00434d2ef8
-##
+##Get Azure AD ID from Azure portal 
+##Get Azure Subscription ID  from Azure portal
 
 
-##log into tenant
-Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"
-
-
-##install module 
+##install module in the powershell 
 Install-Module -Name Microsoft.RDInfra.RDPowerShell
 
 ##import module 
@@ -18,52 +12,56 @@ Import-Module -Name Microsoft.RDInfra.RDPowerShell
 ##update module
 Update-Module -Name Microsoft.RDInfra.RDPowerShell
 
+
+##log into tenant
+Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"
+
 ##get rds wvd tenent
 Get-RdsTenant
  
 ##to remove rds wvd tenant
-Remove-RdsTenant -Name "vdi-test-us"
+Remove-RdsTenant -Name "tenant name"
 
 ## to list the hostpools inside a tenant 
-Get-RdsHostPool -TenantName "wvd-tenant2-demo"
+Get-RdsHostPool -TenantName "your tenant name"
 
-##list the servers inside a host pool
-Get-RdsSessionHost -TenantName "vdi-test-us" -HostPoolName "vdi-hp"
+##list the servers inside a hostpool
+Get-RdsSessionHost -TenantName "your tenant name" -HostPoolName "your hostpool name"
 
 ##to get the appgroups attached to a host pool
-Get-RdsAppGroup -TenantName "wvd-tenant2-demo" -HostPoolName "wvd2hp"
+Get-RdsAppGroup -TenantName "your tenant name" -HostPoolName "your hostpool name"
 
 
 ##to remove appgrp from hostpool
-Remove-RdsAppGroup -TenantName "wvd-tenant2-demo" -HostPoolName "wvd2hp" -Name "demoapp"
+Remove-RdsAppGroup -TenantName "your tenant name" -HostPoolName "your hostpool name" -Name "your app group name"
 
 
 ##to remove host  pool from a tenant
-Remove-RdsHostPool -TenantName "wvd-tenant-demo" -Name "wvdhpdemo"
+Remove-RdsHostPool -TenantName "your tenant name" -Name "your hostpool name"
 
 
 ##to get RDS user session
-Get-RdsUserSession -TenantName "wvd-tenant-demo" -HostPoolName "wvdhpdemo"
+Get-RdsUserSession -TenantName "youe tenant name" -HostPoolName "your hostpool name"
  
 
 
  ##disable and enable new session to a session host
- Set-RdsSessionHost -TenantName "wvd-tenant2-demo" -HostPoolName "wvd2hp" -Name "wvd2fvm-0.adfolks.com" -AllowNewSession $true
+ Set-RdsSessionHost -TenantName "your tenant name" -HostPoolName "your hostpool name" -Name "session server name" -AllowNewSession $true
 
 
  ##to remove user frpm app group
- Remove-RdsAppGroupUser -TenantName "wvd-tenant-demo" -HostPoolName "wvdhpdemo" -AppGroupName "Desktop Application Group" -UserPrincipalName "usertest002@adfolks.com"
+ Remove-RdsAppGroupUser -TenantName "your tenant name" -HostPoolName "your hostpool name" -AppGroupName "Desktop Application Group" -UserPrincipalName "UPN in azure AD"
 
 
  ## Add Users to Default Remote Desktop Group.
-Add-RdsAppGroupUser -TenantName vdi-test-us -HostPoolName vdi-hp -AppGroupName "Desktop Application Group" -UserPrincipalName test1@adfolks.onmicrosoft.com
+Add-RdsAppGroupUser -TenantName "your tenant name" -HostPoolName "your hostpool name" -AppGroupName "Desktop Application Group" -UserPrincipalName "UPN in azure AD"
  
 
  ##to get the RDS session host
- Get-RdsSessionHost -TenantName "wvd-tenant-demo" -HostPoolName "wvdhpdemo"
+ Get-RdsSessionHost -TenantName "your tenant name" -HostPoolName "your hostpool name"
 
 ##force to remove the session host
-Remove-RdsSessionhost -TenantName "wvd-tenant-demo" -HostPoolName "wvdhpdemo" -Name "wvd-demo-vm-0.adfolks.com" -Force
+Remove-RdsSessionhost -TenantName "your tenent name" -HostPoolName "your hospool name" -Name "session host name" -Force
 
 
 Remove-RdsAppGroup -TenantName "wvd-tenant-demo" -HostPoolName "wvdhpdemo" -Name "Desktop Application Group"
